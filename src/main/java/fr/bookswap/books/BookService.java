5ff1321e4f4423ffb8946a874e7888168c1e19d2
T.M <theo.mazzocco@ifts-sls.com>
2026-03-10 20:49:30 +0100

    First batch of Book feature implementation
    The feature is not complete, only half of Ressources has been done. Still this can be reviewed to see a better implementation in the following commits.

--- src/main/java/fr/bookswap/books/BookService.java
+++ src/main/java/fr/bookswap/books/BookService.java
@@ -1,4 +1,67 @@
 package fr.bookswap.books;
 
+import fr.bookswap.books.dto.UpdateBookDto;
+import fr.bookswap.common.entity.Author;
+import fr.bookswap.common.entity.Book;
+import fr.bookswap.common.entity.Review;
+import fr.bookswap.common.entity.User;
+import fr.bookswap.common.exception.NotFoundException;
+import fr.bookswap.common.security.JwtService;
+import fr.bookswap.review.ReviewRepository;
+import io.quarkus.security.ForbiddenException;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
 public class BookService {
+
+    @Inject
+    BookRepository bookRepository;
+
+    @Inject
+    JwtService jwtService;
+
+    public Book getBookById(Long id) {
+        return bookRepository.findByIdOptional(id)
+                .orElseThrow(() -> new NotFoundException(id));
+    }
+
+    @Transactional
+    public List<Book> getAllBooks() {
+        return bookRepository.listAll();
+    }
+
+    @Transactional
+    public Book createBook(Book book) {
+        bookRepository.persist(book);
+        return book;
+    }
+
+    @Transactional
+    public Book updateBookById(UpdateBookDto bookUpdate) {
+        Long currentUserId = Long.valueOf(jwtService.getSubject());
+        return bookRepository.update(currentUserId, bookUpdate);
+    }
+
+    @Transactional
+    public void deleteBook(Long id) {
+        Long currentUserId = jwtService.getUserId();
+        boolean deleted = bookRepository.deleteIfOwned(currentUserId, id);
+        if (!deleted) {
+            throw new ForbiddenException("You do not have permission to delete this book.");
+        }
+    }
+
+    @Transactional
+    public List<Author> getBookAuthors(Long id) {
+        Book book = bookRepository.findById(id);
+        if (book == null) {
+            throw new ForbiddenException("Unkown book id: " + id);
+        }
+        return new ArrayList<>(book.authors);
+    }
 }
