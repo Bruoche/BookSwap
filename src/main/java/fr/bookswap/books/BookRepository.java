540e383d2112283af988e3bb44f92aa73525cd8a
T.M <theo.mazzocco@ifts-sls.com>
2026-03-14 15:30:43 +0100

    Move object ownership check and deletion into the Service

--- src/main/java/fr/bookswap/books/BookRepository.java
+++ src/main/java/fr/bookswap/books/BookRepository.java
@@ -1,23 +1,7 @@
 package fr.bookswap.books;
 
-import fr.bookswap.books.dto.UpdateBookRequest;
 import fr.bookswap.common.entity.Book;
-import fr.bookswap.common.security.JwtService;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import io.quarkus.security.ForbiddenException;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 
 public class BookRepository implements PanacheRepository<Book> {
-
-    @Inject
-    JwtService jwt;
-
-
-    @Transactional
-    public boolean deleteIfOwned(Long currentUserId, Long id) {
-        long deletedCount = delete("id = ?1 and ownerId = ?2", id, currentUserId);
-
-        return deletedCount > 0;
-    }
 }
