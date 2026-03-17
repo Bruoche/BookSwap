e9d6f59aecb07bdf9c457e6095d24c4d80ce2de0
Alice Mailhes <armand.mailhes@gmail.com>
2026-03-17 21:39:42 +0100

    Adding getter and patch for user

--- src/main/java/fr/bookswap/admin/AdminResource.java
+++ src/main/java/fr/bookswap/admin/AdminResource.java
@@ -1,41 +1,39 @@
 package fr.bookswap.admin;
 
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
+
+import fr.bookswap.admin.dto.UserDto;
 
 @Path("/api/admin")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed({"ADMIN"})  // Tous les endpoints nécessitent USER ou ADMIN
 public class AdminResource {
+
+	@Inject
+	AdminService adminService;
 
     @GET
     @Path("/users")
-    public Response getUsers() { //TODO
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(Map.of(
-                        "timestamp", LocalDateTime.now().toString(),
-                        "status", 500,
-                        "error", "Not implemented.",
-                        "message", "This request has not yet been implemented"
-                ))
-                .build();
+    public List<UserDto> getUsers() {
+		return adminService.getAllUsers()
+			.stream()
+			.map(user -> UserDto.fromUser(user))
+			.toList();
     }
 
     @PATCH
     @Path("/users/{id}/suspend")
-    public Response suspendUser() { //TODO
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(Map.of(
-                        "timestamp", LocalDateTime.now().toString(),
-                        "status", 500,
-                        "error", "Not implemented.",
-                        "message", "This request has not yet been implemented"
-                ))
-                .build();
+    public UserDto suspendUser(@PathParam("id") Long id) {
+        return adminService.suspendUser(id);
     }
 
     @DELETE
--- src/main/java/fr/bookswap/admin/AdminService.java
+++ src/main/java/fr/bookswap/admin/AdminService.java
@@ -1,4 +1,37 @@
 package fr.bookswap.admin;
 
+import java.util.List;
+
+import fr.bookswap.admin.dto.UserDto;
+import fr.bookswap.common.entity.User;
+import fr.bookswap.common.exception.NotFoundException;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
 public class AdminService {
+
+	@Inject
+	UserRepository userRepository;
+
+	public List<User> getAllUsers() {
+		return userRepository.listAll();
+	}
+
+	@Transactional
+	public UserDto suspendUser(Long id) {
+		User user = getUserById(id);
+		user.active = false;
+		userRepository.persist(user);
+		return UserDto.fromUser(user);
+	}
+
+	public User getUserById(Long id) {
+		User user = userRepository.findById(id);
+		if (user == null) {
+			throw new NotFoundException(id);
+		}
+		return user;
+	}
 }
