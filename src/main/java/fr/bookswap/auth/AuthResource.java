2992c8dc3d37a95c6fdcb4c43cc0bf71ccba8705
Alice Mailhes <armand.mailhes@gmail.com>
2026-03-22 16:22:33 +0100

    Implementing token refresh

--- src/main/java/fr/bookswap/auth/AuthResource.java
+++ src/main/java/fr/bookswap/auth/AuthResource.java
@@ -2,6 +2,7 @@ package fr.bookswap.auth;
 
 import fr.bookswap.common.entity.User;
 import fr.bookswap.auth.dto.EditUserRequest;
+import fr.bookswap.common.security.Token;
 import fr.bookswap.auth.dto.LoginRequest;
 import fr.bookswap.auth.dto.LoginResponse;
 import fr.bookswap.auth.dto.RegisterRequest;
@@ -12,7 +13,10 @@ import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import java.time.LocalDateTime;
@@ -23,7 +27,12 @@ import java.util.Map;
 @Consumes(MediaType.APPLICATION_JSON)
 public class AuthResource {
 
-    private static final Logger LOG = Logger.getLogger(AuthResource.class);
+    private static final String COOKIE_REFRESH_TOKEN = "refreshToken";
+
+	private static final Logger LOG = Logger.getLogger(AuthResource.class);
+
+	@ConfigProperty(name = "jwt.refresh.expiration.time", defaultValue = "604800")
+	Long refreshExpirationTime;
 
     @Inject
     AuthService authService;
@@ -34,11 +43,10 @@ public class AuthResource {
     @POST
     @Path("/login")
     @PermitAll  // Accessible sans token
-    public LoginResponse login(@Valid LoginRequest request) {
+    public Response login(@Valid LoginRequest request) {
         LOG.info("Tentative de connexion : " + request.username);
-        String token = authService.login(request.username, request.password);
-        User user = User.findByUsername(request.username);
-        return new LoginResponse(token, user.username, user.roles);
+        Token token = authService.login(request.username, request.password);
+		return buildAuthResponse(token);
     }
 
     /**
@@ -76,14 +84,22 @@ public class AuthResource {
 
     @POST
     @Path("/refresh")
-    public Response refreshToken() { //TODO
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(Map.of(
-                        "timestamp", LocalDateTime.now().toString(),
-                        "status", 500,
-                        "error", "Not implemented.",
-                        "message", "This request has not yet been implemented"
-                ))
-                .build();
+    public Response refreshToken(@CookieParam(COOKIE_REFRESH_TOKEN) String refreshToken) {
+		Token token = authService.refreshAuth(refreshToken);
+        return buildAuthResponse(token);
     }
+
+	private Response buildAuthResponse(Token token) {
+        LoginResponse response = new LoginResponse(token.authToken, token.user.username, token.user.roles);
+		NewCookie cookie = new NewCookie.Builder(COOKIE_REFRESH_TOKEN)
+			.value(token.refreshToken)
+			.path("api/auth/refresh")
+			.httpOnly(true)
+			.secure(true)
+			.maxAge(refreshExpirationTime.intValue())
+			.build();
+		return Response.ok(response)
+			.cookie(cookie)
+			.build();
+	}
 }
