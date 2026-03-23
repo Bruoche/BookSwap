8ff6cfa568ee6dd917c83aab486db9b094184217
Alice Mailhes <armand.mailhes@gmail.com>
2026-03-23 23:59:12 +0100

    paginating Library

--- src/main/java/fr/bookswap/common/repository/LibraryRepository.java
+++ src/main/java/fr/bookswap/common/repository/LibraryRepository.java
@@ -9,16 +9,16 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class LibraryRepository  implements PanacheRepository<UserBook> {
 
-	public List<UserBook> listByStatus(UserBook.Status status, Long userId) {
-		return list("(?1 is null or ?1 = status) and ?2 = user.id", status, userId);
+	public List<UserBook> listByStatus(UserBook.Status status, Long userId, int index, int pageSize) {
+		return find("(?1 is null or ?1 = status) and ?2 = user.id", status, userId).page(index, pageSize).list();
 	}
 
 	public UserBook findForUser(Long id, Long userId) {
 		return find("?1 = id and ?2 = user.id", id, userId).firstResult();
 	}
 
-	public List<UserBook> listForUser(String username) {
-		return list("user.username", username);
+	public List<UserBook> listForUser(String username, int index, int pageSize) {
+		return find("user.username", username).page(index, pageSize).list();
 	}
 
 }
