e2f10ee17e3d8d99f9707ae122c85260749a633e
Alice Mailhes <armand.mailhes@gmail.com>
2026-03-24 23:14:22 +0100

    Refactor: Suppression code mort

--- src/main/java/fr/bookswap/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.bookswap.user;
-
-import fr.bookswap.common.entity.User;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-
-public class UserRepository implements PanacheRepository<User> {
-}
